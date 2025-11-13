8c2954a6d23debd79f92675f14db1d04a6f34d61
william <notbinc@icloud.com>
2025-11-13 09:53:28 -0500

    week 13 wor

--- chapters/06_1_lesson/exercises.py
+++ chapters/06_1_lesson/exercises.py
@@ -2,39 +2,72 @@
 ##### Template for Chapter 5.14, Exercises 1 - 4 ######
 
 
-print("********** Ch 6 Exercise 1 **********")
+# print("********** Ch 6 Exercise 1 **********")
 
-print("""
-Type your work for Exercise 1 here
+def b(z):
+    print(f"b z={z}")
+    prod = a(z, z)
+    print(z, prod)
+    return prod
 
-Ch 6 Exercise 1: Not implemented # Delete this line when you write your code!
-""")
+def a(x, y):
+    print(f"a x={x} y={y}")
+    x = x + 1
+    return x * y
+
+def c(x, y, z):
+    print(f"c x={x} y={y} z={z}")
+    total = x + y + z
+    square = b(total)**2
+    return square
+
+x = 1
+y = x + 1
+print(c(x, y+3, x+y))
 
 
-print("********** Ch 6 Exercise 2 **********")
 
-# Do your work for Excercise 2 here.
-
-print("Ch 6 Exercise 2: Not implemented") # Delete this line when you write your code!
+# print("********** Ch 6 Exercise 2 **********")
 
 
+def acc(m,n):
+    if m == 0:
+        return n+1
+    
+    elif m>0 and n==0:
+        return acc(m-1, 1)
 
-print("********** Ch 6 Exercise 3 **********")
+    elif m>0 and n>0:
+        return acc(m-1, acc(m,n-1))
+
+m=2
+n=3
+result = acc(m,n)
+print(f"acc({m},{n}) = {result}")
+
+# print("********** Ch 6 Exercise 3 **********")
 
 # Exercise 3 should be worked in a new file called palindrome.py
 
 
 
-print("********** Ch 6 Exercise 4 **********")
+# print("********** Ch 6 Exercise 4 **********")
 
 # Do your work for Exercise 4 here.
 
-print("Ch 6 Exercise 4: Not implemented") # Delete this line when you write your code!
+# print("Ch 6 Exercise 4: Not implemented") # Delete this line when you write your code!
 
 
 
-print("********** Ch 6 Exercise 5 **********")
+# print("********** Ch 6 Exercise 5 **********")
 
 # Do your work for Exercise 5 here.
 
-print("Ch 6 Exercise 5: Not implemented") # Delete this line when you write your code!
+
+
+
+
+
+
+
+
