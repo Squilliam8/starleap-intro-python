869f038b7dbc3282b2b2c2926ac8705e31a99ab8
william <notbinc@icloud.com>
2025-12-04 09:55:47 -0500

    week14 work

--- chapters/04_1_lesson/play.py
+++ chapters/04_1_lesson/play.py
@@ -1,23 +1,59 @@
 import turtle
+import time 
+import random
 
-o = turtle.Turtle()
+screen = turtle.Screen()
+screen.setup(800, 800)
+turtle.tracer(False)
 
-o.color('purple','pink')
-o.shape('turtle')
-o.fd(1)
-o.lt(100)
-o.fd(5)
-o.lt(30)
-o.fd(100)
-o.lt(50)
-o.fd(300)
-o.lt(10)
-o.fd(8)
-o.lt(360)
-o.fd(100)
-o.lt(360)
-o.lt(100000)
+turtles: list[turtle.Turtle] = []
+
+
+X_BOUND=300
+Y_BOUND=300
+
+def draw_bounds():
+    t = turtle.Turtle()
+    t.goto(X_BOUND, Y_BOUND)
+    t.pendown()
+    t.goto(X_BOUND, -Y_BOUND)
+    t.goto(-X_BOUND, -Y_BOUND)
+    t.goto(-X_BOUND, Y_BOUND)
+    t.goto(X_BOUND, Y_BOUND)
+    t.hideturtle()
+    screen.update()
+draw_bounds()
+
+
+def add_turtle(x, y):
+    global turtles
+    t = turtle.Turtle()
+    turtle.colormode(255)
+    t.color((random.randint(0,255), random.randint(0, 255), random.randint(0, 255)))
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    t.shape('turtle')
+    turtles.append(t)
+screen.onclick(add_turtle)
+
+
+
+while True:
+    for t in turtles:
+        (old_x, old_y) =t.position()
+        possible_moves = [t.forward, t.back, t.right, t.left]
+        random_value = random.randint(0, 100)
+        random.choice(possible_moves)(random_value)
+        (new_x, new_y) = t.position()
+        if (new_x > X_BOUND or new_x < -X_BOUND or new_y > Y_BOUND or new_y < -Y_BOUND):
+            t.goto(old_x, old_y)
+    
+    
+    time.sleep(0)
+    screen.update()
+
+
+
 
 turtle.mainloop()
-o.shape('turtle')
-
