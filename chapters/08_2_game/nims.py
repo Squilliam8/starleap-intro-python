3d3c9fdb46844106a71e1a99f70d66139e61a007
william <notbinc@icloud.com>
2026-03-26 09:50:53 -0400

    msg week nimsyes

--- chapters/08_2_game/nims.py
+++ chapters/08_2_game/nims.py
@@ -1,16 +1,37 @@
+min_stones = 1
 pile = 50
 max_stones = 5
-
-answer = int(input ('1-5 roc'))
-
-if answer == (1,2,3,4,5):
-    pile -= answer
-    print(pile)
-
-    
+player = 1
 
 
 
+def valid_answer():
+    while True:
+        guess_text=input(f"player{player} how many stones do you want to take") 
+        try:
+            guess = int(guess)
+            if guess > max_stones or guess < min_stones:
+                raise ValueError()
+            return guess
+        except:
+            print("who do you think you are you little brat🧌")
+
+
+def play_nihmsyes(pile, max_stones):
+        global player
+        print (f"there are (pile) nimsyes")
+        while pile >0:
+             answer = valid_answer()
+             pile -= answer
+             if player == 1:
+                  player = 2
+             else:
+                  player = 1
+        if pile > 0 and player == 1:
+             print("PLAYER ONE WINS NIMSYES🎊")
+        else:
+             print("player two takes the nimsyes")
+             
 
 
 
@@ -21,9 +42,8 @@ if answer == (1,2,3,4,5):
 
 
 
-# def play_nims(pile, max_stones):
-#pile w/ 100
-# ask p1 how many 1-5
+
+
 #take stones
 # ask p2
 # take
